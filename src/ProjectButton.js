3918fa1d5d5e159b86a061e40f587fe902104a76
Ryan <mathewsthomasryan@gmail.com>
2024-04-05 16:30:31 -0500

    hdf

--- src/ProjectButton.js
+++ src/ProjectButton.js
@@ -1,6 +1,13 @@
-function heyman() {
-    const message = "heyman";
-    alert(message);
-};
+function ProjectButton() {
+    const handleClick = () => {
+        
+    }
+    const projectBtn = document.querySelector("#new-project-btn");
+    projectBtn.addEventListener("click", handleClick);
+}
+// function heyman() {
+//     const message = "heyman";
+//     alert(message);
+// };
 
-module.exports = {heyman};
+// module.exports = {heyman};
