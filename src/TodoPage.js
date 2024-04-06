86dbae031ce5551c9a10069c97501c379696b356
Ryan <mathewsthomasryan@gmail.com>
2024-04-06 15:19:48 -0500

    try

--- src/TodoPage.js
+++ src/TodoPage.js
@@ -1,3 +1,4 @@
 import "./style.css";
-import { Project } from "./make-project";
+import { makeProject } from "./make-project";
 
+makeProject();
---
+++ src/make-project.js
@@ -0,0 +1,20 @@
+export function makeProject () {
+    let allProjects = [];
+    let projName = document.querySelector("#proj-name").value;
+    let projConfirm = document.querySelector(".proj-confirm");
+    let projModal = document.querySelector(".project-modal");
+    let projClose = document.querySelector(".proj-close-btn");
+    let projList = document.querySelector(".project-list");
+    
+    const projBtn = document.querySelector("#new-project-btn");
+    projBtn.addEventListener("click", () => {
+        projModal.style.display = "block";
+    });
+    projClose.addEventListener("click", () => {
+        projModal.style.display = "none";
+    });
+    projConfirm.addEventListener("click", () => {
+        allProjects.push(projName);
+    });
+
+};
