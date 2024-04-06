5b5da739ea7b16468096fb61d4af610532132990
Ryan <mathewsthomasryan@gmail.com>
2024-04-06 15:43:02 -0500

    dfg

--- src/make-project.js
+++ src/make-project.js
@@ -1,20 +1,22 @@
-export function makeProject () {
-    let allProjects = [];
-    let projName = document.querySelector("#proj-name").value;
-    let projConfirm = document.querySelector(".proj-confirm");
-    let projModal = document.querySelector(".project-modal");
-    let projClose = document.querySelector(".proj-close-btn");
-    let currentProj = document.querySelector(".project-list");
+// export function makeProject () {
+//     let allProjects = [];
+//     let projName = document.querySelector("#proj-name").value;
+//     let projConfirm = document.querySelector(".proj-confirm");
+//     let projModal = document.querySelector(".project-modal");
+//     let projClose = document.querySelector(".proj-close-btn");
+//     let currentProj = document.querySelector(".project-list");
     
-    const projBtn = document.querySelector("#new-project-btn");
-    projBtn.addEventListener("click", () => {
-        projModal.style.display = "block";
-    })
-    projClose.addEventListener("click", () => {
-        projModal.style.display = "none";
-    })
-    projConfirm.addEventListener("click", () => {
-        allProjects.push(projName);
-    })
-    allProjects.innerHTML = projName;
-};
+//     const projBtn = document.querySelector("#new-proj-btn");
+//     projBtn.addEventListener("click", () => {
+//         projModal.style.display = "block";
+//     })
+//     projClose.addEventListener("click", () => {
+//         projModal.style.display = "none";
+//     })
+//     projConfirm.addEventListener("click", () => {
+//         allProjects.push(projName);
+//     })
+//     return {
+//         allProjects
+//     };
+// };
