0db88b835bdf8642d6443fb5df68e4d98f721616
Ryan <mathewsthomasryan@gmail.com>
2024-04-07 21:01:09 -0500

    disp

--- src/make-project.js
+++ src/make-project.js
@@ -1,14 +1,36 @@
 let allProjects = [];
 let projModal = document.querySelector(".project-modal")
+let projClose = document.querySelector(".proj-close-btn");
 let newProj = document.querySelector("#new-proj-btn");
+let projConfirm = document.querySelector(".proj-confirm");
+
+projClose.addEventListener("click", () =>{
+    projModal.style.display = "none";
+});
+
 newProj.addEventListener("click", () => {
     projModal.style.display = "block";
 });
 
-function Project() {
-
+export function Project(name, description, complete) {
+    this.name = name;
+    this.description = description;
+    this.complete = complete;
 }
 
-function addProject() {
-
+export function render() {
+    
 }
+
+export function addProject() {
+    let projName = document.querySelector("#proj-name").value;
+    let projDesc = document.querySelector("#proj-description").value;
+    let projComplete = document.querySelector("#proj-complete");
+    let makeProj = new Project(projName, projDesc, projComplete);
+    allProjects.push(makeProj);
+    render();
+}
+
+projConfirm.addEventListener("click", () => {
+    addProject()
+});
